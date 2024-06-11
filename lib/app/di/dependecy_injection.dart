ad7d586b24427575d7f2e1cd8e333be2340e37fb
Waleed Bin Shehab <waleedbinshihab@gmail.com>
2024-06-11 22:05:36 +0300

    hide app url

--- lib/app/di/dependecy_injection.dart
+++ lib/app/di/dependecy_injection.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shawir/config/api.dart';
 import 'package:shawir/data/api/request_expert/request_expert_api.dart';
 import 'package:shawir/data/dio/factory.dart';
 import 'package:shawir/data/dio/methods.dart';
@@ -15,6 +17,9 @@ import '../compressor/compressor.dart';
 abstract class DependecyInjection {
   static final GetIt instance = GetIt.instance;
   static Future<void> initializeApp() async {
+    await dotenv.load();
+    ApiConfig.baseUrl = dotenv.env['APP_URL'] ?? "APP_URL";
+    ApiConfig.authToken = dotenv.env['TOKEN'] ?? "TOKEN";
     await GetStorage.init();
     Get.lazyPut<Dio>(() => DioFactory().getDio);
     Get.lazyPut<DioMethods>(() => DioMethods(Get.find<Dio>()));
